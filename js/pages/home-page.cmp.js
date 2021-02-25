c227152ef537b611d0cd7a76cb25a869f431560d
avi lugassi <avilugassi23@gmail.com>
2021-02-25 17:07:14 +0200

    fixed responsivnes for mail books and main

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <section class="home" >
     <div class="main-apsus">
-        <router-link to="/book" class="apsus book "></router-link> |
+        <router-link to="/book" class="apsus book "></router-link> 
            <router-link to="/keep" class="apsus keep"></router-link> 
            <router-link to="/email" class="apsus email"></router-link> 
 </div>
